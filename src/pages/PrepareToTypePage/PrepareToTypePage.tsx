7d659990eecc572a8f033a86708e9f177631ab7f
Khasanov Petr <n0sfer666@yandex.ru>
2022-08-18 10:12:52 +0300

    fixed press ANY key, typed text overflow

--- src/pages/PrepareToTypePage/PrepareToTypePage.tsx
+++ src/pages/PrepareToTypePage/PrepareToTypePage.tsx
@@ -19,6 +19,7 @@ export default function PrepareToTypePage() {
         type="button"
         className={styles.button}
         onClick={handleButtonClick}
+        onKeyDown={handleButtonClick}
         onBlur={({ target }) => target.focus()}
         autoFocus
       >
